52b2107dd47269845af84703fc77be20d2f6d305
Roman Nezelski <nezelski@gmail.com>
2025-11-23 20:05:46 +0200

    Completed tasks from lesson 17

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -1,33 +1,33 @@
-import { Locator, Page } from '@playwright/test'
+import type { Locator, Page } from '@playwright/test'
 import { OrderPage } from './order-page'
 import { SERVICE_URL } from '../../config/env-data'
 
 export class LoginPage {
-  readonly page: Page
-  readonly url: string = SERVICE_URL
-  readonly signInButton: Locator
-  readonly usernameField: Locator
-  readonly passwordField: Locator
-  // add more locators here
+    readonly page: Page
+    readonly url: string = SERVICE_URL
+    readonly signInButton: Locator
+    readonly usernameField: Locator
+    readonly passwordField: Locator
+    readonly errorMessagePopup: Locator
+    // add more locators here
 
-  constructor(page: Page) {
-    this.page = page
-    this.signInButton = page.getByTestId('signIn-button')
-    this.usernameField = page.getByTestId('username-input')
-    this.passwordField = page.getByTestId('password-input')
-    // continue with the rest of the implementation below
-  }
+    constructor(page: Page) {
+        this.page = page
+        this.signInButton = page.getByTestId('signIn-button')
+        this.usernameField = page.getByTestId('username-input')
+        this.passwordField = page.getByTestId('password-input')
+        this.errorMessagePopup = page.getByTestId('authorizationError-popup')
+    }
 
-  async open() {
-    await this.page.goto(this.url)
-  }
+    async open() {
+        await this.page.goto(this.url)
+    }
 
-  async signIn(username: string, password: string) {
-    await this.usernameField.fill(username)
-    await this.passwordField.fill(password)
-    await this.signInButton.click()
-    return new OrderPage(this.page)
-  }
+    async signIn(username: string, password: string) {
+        await this.usernameField.fill(username)
+        await this.passwordField.fill(password)
+        await this.signInButton.click()
 
-  // continue with the rest of the implementation below
+        return new OrderPage(this.page)
+    }
 }
--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -3,18 +3,17 @@ import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-test('signIn button disabled when incorrect data inserted', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  await loginPage.open()
-  await loginPage.usernameField.fill(faker.lorem.word(2))
-  await loginPage.passwordField.fill(faker.lorem.word(7))
-  await expect(loginPage.signInButton).toBeDisabled()
+test('TL-17-1 signIn button disabled when incorrect data inserted', async ({ page }) => {
+    const authPage = new LoginPage(page)
+    await authPage.open()
+    await authPage.usernameField.fill(faker.lorem.word(2))
+    await authPage.passwordField.fill(faker.lorem.word(7))
+    await expect(authPage.signInButton).toBeDisabled()
 })
 
-test('login with correct credentials and verify order creation page', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  await loginPage.open()
-  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.statusButton.click({ force: true })
-  // verify at least few elements on the order creation page
+test('TL-17-2 login with correct credentials and verify order creation page', async ({ page }) => {
+    const authPage = new LoginPage(page)
+    await authPage.open()
+    const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+    await orderCreationPage.checkInnerComponentsVisible()
 })
